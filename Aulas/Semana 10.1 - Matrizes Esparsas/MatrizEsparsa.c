e44baecabe076b9e6bda7c204516573d8730f25e
João Assaoka <joaovictorar11+GitHub@gmail.com>
2023-12-02 23:19:07 -0300

    Update MatrizEsparsa.c

--- Aulas/Semana 10.1 - Matrizes Esparsas/MatrizEsparsa.c
+++ Aulas/Semana 10.1 - Matrizes Esparsas/MatrizEsparsa.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definição de tipos:
 #define MAX_LINHA 10
 typedef struct no {
     int valor, coluna; // valor e coluna do elemento
@@ -9,3 +10,19 @@ typedef struct no {
 typedef SNo *PNo; // ponteiro para SNo
 typedef PNo PME[MAX_LINHA]; // Vetor de ponteiros para guardar o endereço do primeiro nó de cada linha
 
+
+// Inicialização:
+void inicializa(PME matriz) {
+    int i;
+    for (i = 0; i < MAX_LINHA; i++) matriz[i] = NULL;
+}
+
+// Inserção:
+
+
+
+// Remoção:
+
+// Busca:
+
+// Impressão:
