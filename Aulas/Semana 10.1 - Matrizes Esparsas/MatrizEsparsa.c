8fd5f1192f30814674f34ec2aafdf1e1c0fa2d18
João Assaoka <joaovictorar11+GitHub@gmail.com>
2023-12-02 23:38:38 -0300

    Update MatrizEsparsa.c

--- Aulas/Semana 10.1 - Matrizes Esparsas/MatrizEsparsa.c
+++ Aulas/Semana 10.1 - Matrizes Esparsas/MatrizEsparsa.c
@@ -14,11 +14,20 @@ typedef PNo PME[MAX_LINHA]; // Vetor de ponteiros para guardar o endereço do pr
 // Inicialização:
 void inicializa(PME matriz) {
     int i;
-    for (i = 0; i < MAX_LINHA; i++) matriz[i] = NULL;
+    for (i = 0; i < MAX_LINHA; i++) matriz[i] = NULL; // inicializa todas as linhas com NULL (vazias)
 }
 
 // Inserção:
+void inserir (PME matriz, int valor, int linha, int coluna) {
+    PNo ant, p, novo = (PNo)malloc(sizeof(SNo));
+    novo->valor = valor;
+    novo->coluna = coluna;
 
+    for (ant = NULL, p = matriz[linha]; p != NULL && coluna > p->coluna; ant = p, p = p->prox); // percorre a linha até encontrar a posição correta
+    novo->prox = p;
+    if (ant == NULL) matriz[linha] = novo; // inserção no início da linha
+    else ant->prox = novo; // inserção no meio ou no final da linha
+}
 
 
 // Remoção:
