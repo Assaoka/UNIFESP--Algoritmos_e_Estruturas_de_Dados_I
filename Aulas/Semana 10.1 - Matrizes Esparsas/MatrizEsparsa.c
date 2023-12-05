96e25a076a3d21205a881e48c6c85e48d9b325bf
João Assaoka <joaovictorar11+GitHub@gmail.com>
2023-12-05 13:01:17 -0300

    Update MatrizEsparsa.c

--- Aulas/Semana 10.1 - Matrizes Esparsas/MatrizEsparsa.c
+++ Aulas/Semana 10.1 - Matrizes Esparsas/MatrizEsparsa.c
@@ -28,7 +28,8 @@ PNo criarNo (int valor, int coluna, PNo prox) { // Aloca um novo nó com os valo
 
 void inserir (PME matriz, int valor, int linha, int coluna) {
     PNo ant, p;
-    for (ant = NULL, p = matriz[linha]; p != NULL && p->coluna < coluna; ant = p, p = p->prox); // percorre a linha até encontrar a posição correta para a coluna
+    if (valor == 0) return; // Se o valor a ser inserido é zero, não faz nada
+    for (ant = NULL, p = matriz[linha]; p != NULL && p->coluna < coluna; ant = p, p = p->prox); // percorre a linha até encontrar a posição correta
     if (p != NULL && p->coluna == coluna) p->valor = valor; // Se já existe um elemento na posição, atualiza o valor
     else if (ant == NULL) matriz[linha] = criarNo(valor, coluna, p); // Inserir no início da linha
     else ant->prox = criarNo(valor, coluna, p); // Inserir no meio ou no final da linha
@@ -36,7 +37,22 @@ void inserir (PME matriz, int valor, int linha, int coluna) {
 
 
 // Remoção:
+void remover (PME matriz, int linha, int coluna) {
+    PNo ant, p;
+    for (ant = NULL, p = matriz[linha]; p != NULL && p->coluna < coluna; ant = p, p = p->prox); // percorre a linha até encontrar a posição correta
+    if (p != NULL && p->coluna == coluna) { // Se existe um elemento na posição
+        if (ant == NULL) matriz[linha] = p->prox; // Se é o primeiro elemento da linha
+        else ant->prox = p->prox; // Se é um elemento do meio ou do final da linha
+        free(p); // Libera o nó
+    }
+}
 
 // Busca:
+int buscar (PME matriz, int linha, int coluna) {
+
+}
 
 // Impressão:
+int imprimir (PME matriz) {
+
+}
